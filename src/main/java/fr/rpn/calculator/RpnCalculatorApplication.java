eb70270ec428205c96caf110979f80d848af5233
hidden <>
2022-09-30 23:56:37 +0100

    project initiation

---
+++ src/main/java/fr/rpn/calculator/RpnCalculatorApplication.java
@@ -0,0 +1,13 @@
+package fr.rpn.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpnCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpnCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/rpn/calculator/RpnCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rpn.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpnCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
